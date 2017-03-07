baf2c4b156b1492d713269d14b6056cbc6478b44
Antanas Kompanas <a.kompanas@baltic-amadeus.lt>
2017-03-07 11:42:59 +0200

    Rewritten Demo Client to support Client Handler.

---
+++ Game/Game.DemoClient/PlayerDTO.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game.DemoClient
+{
+    public class PlayerReq
+    {
+        public string GameUid;
+        public int Turn;
+
+        public int YourIndex;
+        public List<EnPlayerState> Players;
+        public EnMapData Map;
+    }
+
+    public class PlayerResp
+    {
+        public EnPoint Turn;
+        public bool Pass;
+    }
+
+    public class EnPlayerState
+    {
+        public string Condition;
+        public string Comment;
+        public int Score;
+    }
+
+    public class EnMapData
+    {
+        public int Width;
+        public int Height;
+        public List<string> Rows;
+    }
+
+    public class EnPoint
+    {
+        public int Row;
+        public int Col;
+    }
+}
--- Game/Game.DemoClient/Program.cs
+++ Game/Game.DemoClient/Program.cs
@@ -1,111 +1,35 @@
 ﻿using System;
 using System.Reflection;
-using Game.DebugClient.DataContracts;
-using Game.DebugClient.Infrastructure;
-using Game.DebugClient.Utilites;
+using Newtonsoft.Json;
+using System.IO;
 
 namespace Game.DemoClient
 {
     public class Program
     {
-        public static IServiceCallInvoker ServiceCallInvoker { get; private set; }
-
         static void Main(string[] args)
         {
             Console.Title = $"Game Demo Bot - ver. {Assembly.GetExecutingAssembly().GetName().Version}";
-            ServiceCallInvoker = new ServiceCallInvoker(new Logger(), new JsonWebServiceClient());
-            ReadParams(args);
-
-            var gameFlowWrapper = new GameFlowWrapper();
-
-            while (true)
+            if (args.Length != 2)
             {
-                var noFail = gameFlowWrapper.InitLogin();
-                if (noFail == false)
-                    break;
-
-                noFail = gameFlowWrapper.CompleteLogin();
-                if (noFail == false)
-                    break;
-
-                noFail = gameFlowWrapper.CreatePlayer();
-                if (noFail == false)
-                    break;
-
-                noFail = gameFlowWrapper.WaitGameStart();
-               
-                while (noFail)
-                {
-                    noFail = gameFlowWrapper.WaitNextTurn();
-                    if (noFail == false)
-                        break;
-
-                    noFail = gameFlowWrapper.GetPlayerView();
-                    if (noFail == false)
-                        break;
-
-                    noFail = gameFlowWrapper.PerformMove();
-                    if (noFail == false)
-                        break;
-                }
-
-                gameFlowWrapper.LeaveGame();
-                break;
+                Console.WriteLine("USAGE: Game.DemoClient inputFileName outputFileName");
+                return;
             }
 
-            Console.ReadKey(true);
+            var playerReq = JsonConvert.DeserializeObject<PlayerReq>(File.ReadAllText(args[0]));
+            var playerResp = PerformMove(playerReq);
+            File.WriteAllText(args[1], JsonConvert.SerializeObject(playerResp));
         }
 
-
-
-        private static void ReadParams(string[] args)
+        private static PlayerResp PerformMove(PlayerReq req)
         {
-            Console.WriteLine(@"Enter server url:");
-            if (args.Length > 0 && !string.IsNullOrWhiteSpace(args[0]))
+            var solver = new GameSolver(req);
+            var resp = new PlayerResp
             {
-                Console.WriteLine(args[0]);
-                Settings.ServerUrl = args[0];
-            }
-            else
-            {
-                Settings.ServerUrl = Console.ReadLine();
-            }
-
-
-            Console.WriteLine(@"Enter Team name:");
-            if (args.Length > 1 && !string.IsNullOrWhiteSpace(args?[1]))
-            {
-                Console.WriteLine(args[1]);
-                Settings.TeamName = args[1];
-            }
-            else
-            {
-                Settings.TeamName = Console.ReadLine();
-            }
-
-            Console.WriteLine(@"Enter UserName:");
-            if (args.Length > 2 && !string.IsNullOrWhiteSpace(args?[2]))
-            {
-                if (args[2] == "[random]") args[2] = "random" + new Random().Next(100);
-                Console.WriteLine(args[2]);
-                Settings.UserName = args[2];
-            }
-            else
-            {
-                Settings.UserName = Console.ReadLine();
-            }
-
-            Console.WriteLine(@"Enter Password:");
-            if (args.Length > 3 && !string.IsNullOrWhiteSpace(args?[3]))
-            {
-                Console.WriteLine(args[3]);
-                Settings.Password = args[3];
-            }
-            else
-            {
-                Settings.Password = Console.ReadLine();
-            }
-
+                Turn = solver.GetTurn()
+            };
+            resp.Pass = (resp.Turn == null);
+            return resp;
         }
     }
 }
