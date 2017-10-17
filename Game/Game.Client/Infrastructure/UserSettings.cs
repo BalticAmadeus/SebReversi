966fe2e5aa05ac17a19687b45d9520caa06e5482
Antanas Kompanas <a.kompanas@baltic-amadeus.lt>
2017-10-17 14:23:48 +0300

    EMEA PUG Challenge colors and images.

--- Game/Game.Client/Infrastructure/UserSettings.cs
+++ Game/Game.Client/Infrastructure/UserSettings.cs
@@ -5,7 +5,7 @@ namespace Game.AdminClient.Infrastructure
 {
     public static class UserSettings
     {
-        private const string TitleTemplate = "US PUG Challenge - v{0}";
+        private const string TitleTemplate = "EMEA PUG Challenge 2017 - v{0}";
 
         public static TeamRole Role { get; set; }
 
